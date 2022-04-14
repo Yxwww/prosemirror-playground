463a183699bff4303edbd9c293dd9b8c25597227
Yuxi Wang <yuxi.wangcd@gmail.com>
2022-04-14 11:08:43 -0700

    Fix codemirror style

--- src/main.ts
+++ src/main.ts
@@ -231,9 +231,3 @@ window.view = new EditorView(document.querySelector('#editor'), {
 // }
 
 import 'codemirror/mode/javascript/javascript'
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
